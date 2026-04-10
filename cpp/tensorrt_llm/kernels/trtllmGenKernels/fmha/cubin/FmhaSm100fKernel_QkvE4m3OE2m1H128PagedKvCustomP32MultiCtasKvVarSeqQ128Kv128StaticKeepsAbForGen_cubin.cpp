bb33a1a4d5597b7614f0cdffc1f4fc2702d296af
qgai <qgai@nvidia.com>
2026-04-10 08:29:15 -0700

    [None][feat] Add FP8 E4M3/E2M1 FMHA cubin kernels for SM100f Blackwell (H64/H128)
    Signed-off-by: qgai <qgai@nvidia.com>

---
+++ cpp/tensorrt_llm/kernels/trtllmGenKernels/fmha/cubin/FmhaSm100fKernel_QkvE4m3OE2m1H128PagedKvCustomP32MultiCtasKvVarSeqQ128Kv128StaticKeepsAbForGen_cubin.cpp
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:b57945690116d7a70b8601e642048a076a92f9c9836018e42b94edaeffbf4165
+size 839334
---
+++ cpp/tensorrt_llm/kernels/trtllmGenKernels/fmha/cubin/FmhaSm100fKernel_QkvE4m3OE2m1H64PagedKvCustomP32MultiCtasKvCgaVarSeqQ128Kv128StaticKeepsAbForGen_cubin.cpp
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:e1356c1b787a68a4faae975e36a228bea842851304ed89e6eb1760555a896b19
+size 810182
